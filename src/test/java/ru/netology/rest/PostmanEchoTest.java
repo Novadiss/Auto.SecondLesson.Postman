ae7c1469c4961964fc7f6ad6dbad3e1c94360bed
Vladimir Palamar <vladimir.palamar@lenta.com>
2023-11-14 16:07:28 +0300

    chore: create .appveyour.eml and Test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,4 +1,23 @@
 package ru.netology.rest;
 
-public class PostmanEchoTest {
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matcher.equalTO;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData () {
+                given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTO("some data"))
+        ;
+    }
+
 }
